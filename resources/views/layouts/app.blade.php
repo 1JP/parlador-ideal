f331005e37f070e7f8204ac69b0a1f5975088e72
joaopgs <joaopedrogs1@outlook.com>
2024-02-03 21:53:22 -0300

    does not show header on login screen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,35 +18,31 @@
 </head>
 <body class="bg-info">
     <div id="app">
-        <header>
-            <div class="py-2 border-bottom border-light">
-              <div class="container">
-                <div class="row d-flex align-items-center justify-content-start">
-                  <div class="col-lg-3 col-12 d-flex justify-content-center logo">
-                    <a href="index.html"><img class="desktop" src="img/logo/logo-com-assinatura.svg"></a>
-                    <a href="index.html"><img class="mobile" src="img/logo/logo-sem-assinatura.svg"></a>
-                  </div>
-                  <div class="col-12 col-sm-12 d-none d-lg-block d-xl-block col-lg-3"></div>
-                  <div class="col-12 col-lg-3"></div>
-                  <div class="d-flex align-items-center px-0 col-12 col-lg-3">
-                    <div class="mr-2 d-none d-lg-block d-xl-block">
-                      <a class="text-dark" href="{{ route('login') }}">Login </a>
+        @if (Route::currentRouteName() != 'login')
+            <nav class="py-2 border-bottom border-light">
+                <div class="d-flex">
+                    <div class="p-2 w-100"></div>
+                    <div class="p-2 flex-shrink-1">
+                        @if (Auth::user())
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button tabindex="0" class="btn btn-primary m-1 flex flex-row items-center">
+                                    <i class="mr-2" data-lucide="user-circle-2"></i>
+                                    <span class="mr-2">Sair</span>
+                                </button>
+                            </form>
+                        @else
+                            <a class="btn btn-primary" href="{{ route('login') }}">Login </a>
+                        @endif
                     </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-            <!-- Menu all categorias - end -->
-            <!-- Menu mobile - end -->
-            <!-- Menu all categorias overlay - start -->
-            <div class="zeynep-overlay"></div>
-            <!-- Menu all categorias overlay - end -->
-        </header>
+            </nav>
+        @endif
+      
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-
     @include('layouts.partials.scripts')
 </body>
 </html>
