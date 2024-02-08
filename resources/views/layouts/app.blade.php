79129582ce8256cfba96ca080067cd076371bc58
joaopgs <joaopedrogs1@outlook.com>
2024-02-08 16:41:17 -0300

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('img/produto.png') }}" />
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
